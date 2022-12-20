d9dd60db61bb2ef2a4592f55ed3b238e786977b8
stevenotieno <steven.omondi@student.moringaschool.com>
2022-12-20 08:42:13 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
-const init = () => {
-  
-}
 
-document.addEventListener('DOMContentLoaded', init);
+
+const init = () => {
+    const inputForm = document.querySelector('form');
+  
+    inputForm.addEventListener('submit', (event) => {
+      event.preventDefault();
+  
+    });
+  }
+  
+  document.addEventListener('DOMContentLoaded', init);
